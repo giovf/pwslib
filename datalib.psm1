d374131c517f0909cc476de08b12cca11a543ccc
giovf <giova1506@hotmail.com>
2023-07-21 10:51:46 +0100

    modified:   datalib.psm1

--- datalib.psm1
+++ datalib.psm1
@@ -8,3 +8,33 @@ function Get-SquaredNumber {
     )
     return $Number * $Number
 }
+
+function Convert-DateFormat {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Date
+    )
+    
+	$formats = @("dd-MM-yyyy", "dd/MM/yyyy", "MM/dd/yyyy", "dd'th' MMMM yyyy", "dd MMMM yyyy", "d/M/yy", "d MMM yyyy")
+
+    foreach ($format in $formats) {
+        try {
+            $parsedDate = [DateTime]::ParseExact($Date, $format, [CultureInfo]::InvariantCulture)
+            return $parsedDate.ToString("dd/MM/yyyy")
+        }
+        catch {
+            continue
+        }
+    }
+
+$originalDate = $args[0]
+$convertedDate = Convert-DateFormat -Date $originalDate
+Write-Host "Converted date: $convertedDate"
+
+Set-Clipboard -Value ($convertedDate)
+
+$windowTitle = "Completed"
+[Console]::Title = $windowTitle
+
+}
+
